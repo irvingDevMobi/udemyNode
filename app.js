b0a737ddbac3602226938a629e312a6602f7b399
Irving Lop <nirving21@gmail.com>
2019-09-29 21:37:30 -0500

    S3-12 Chalk library test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const validator = require('validator')
 const getNotes = require("./notes.js")
+const chalk = require('chalk')
 
 const message = getNotes()
 
@@ -7,3 +8,7 @@ console.log(message)
 
 console.log(validator.isEmail("nirving21@gmail.com"))
 console.log(validator.isEmail("irvingAtgmail.com"))
+
+console.log(chalk.green("Success.."))
+console.log(chalk.bold("This letter is bold"))
+console.log(chalk.inverse.green("Green background"))
