dd9e33f94d2feee2d21af6844af7290ab836aec1
Irving Lop <nirving21@gmail.com>
2019-09-29 20:39:17 -0500

    S3-11 Using validator package

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const getNotes = require("./notes.js")
 const message = getNotes()
 
 console.log(message)
+
+console.log(validator.isEmail("nirving21@gmail.com"))
+console.log(validator.isEmail("irvingAtgmail.com"))
