8314b680cb934c2c7de4de8b924ceb52ac031f55
Irving Lóp <nirving21@gmail.com>
2019-10-18 09:27:39 -0500

    S5 Refactoring folders

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -10,6 +10,9 @@ const addNote = (title, body) => {
     const duplicateNote = notes.find((note) =>
         note.title === title
     )
+
+    debugger
+
     if (!duplicateNote) {
         notes.push({
             title: title,
