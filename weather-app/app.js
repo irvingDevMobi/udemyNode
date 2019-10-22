8077d6bbd6cc94eb247561c7b608080cd3149731
Irving Lóp <nirving21@gmail.com>
2019-10-21 20:06:09 -0500

    S6-L29 Asynchronous basics

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('2 second timer')
+}, 2000)
+
+setTimeout(() => {
+    console.log(' 0 seconds Timer')
+}, 0)
+
+console.log('Stopping')
