9255489ab8ba4547927a2300cf1f01056b4ae8c0
Irving Lóp <nirving21@gmail.com>
2019-10-28 16:51:08 -0600

    S6-L35 Callback fuction challenge

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -20,3 +20,14 @@ const geocode = (address, callback) => {
 geocode('México', (data) => {
     console.log(data)
 })
+
+const add = (num1, num2, callback) => {
+    setTimeout(() => {
+        const sum = num1 + num2
+        callback(sum)
+    }, 2000)
+}
+
+add(1, 4, (sum) => {
+    console.log(sum)
+})
