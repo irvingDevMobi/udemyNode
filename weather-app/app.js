d64a949fc9276e3fa0356427b39c859551a4c014
Irving Lóp <nirving21@gmail.com>
2019-10-28 18:12:54 -0600

    S6-L37 Callback Abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request')
-
+const geocode = require('./utils/geocode')
+/*
 const url = 'https://api.darksky.net/forecast/e39d7d10ad3c69f1cd77197a9c1477fd/37.8267,-122.4233'
 const mapsApi = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibmlydmluZzIxIiwiYSI6ImNrMjJjZzdrOTF0ODQzbXFvdzB2cTF2aHIifQ.A5PXu49KPC9cs61vir4x0A&limit=1'
 
@@ -23,4 +24,11 @@ request({ url: mapsApi, json: true }, (error, response) => {
             console.log(point[0] + ',' + point[1])
         }
     }
+})
+*/
+
+
+geocode('Neza', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibmlydmluZzIxIiwiYSI6ImNrMjJjZzdrOTF0ODQzbXFvdzB2cTF2aHIifQ.A5PXu49KPC9cs61vir4x0A&limit=1'
+
+    request({ uri: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find location. Try another search', undefined)
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
