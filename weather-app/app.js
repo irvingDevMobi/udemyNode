5ea12629301a5a563413d83698d1f91162fe4584
Irving Lóp <nirving21@gmail.com>
2019-10-29 17:40:02 -0600

    S6-L38 Callbak Chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,11 +3,14 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 geocode('Neza', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
-
-forecast(37.8267, -122.4233, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+    if (error) {
+        return console.log(error)
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(data.location)
+        console.log(forecastData)
+    })
 })
