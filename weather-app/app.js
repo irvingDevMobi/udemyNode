edeb71b1a0a7e97de1384696b630eca106e6fc94
Irving Lóp <nirving21@gmail.com>
2019-10-29 19:54:19 -0600

    S6-L38 Accept city by command Line - Me

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,15 +2,22 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Neza', (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+var myArgs = process.argv.slice(2)
+
+if (myArgs.length > 0) {
+
+    geocode(myArgs[0], (error, data) => {
         if (error) {
             return console.log(error)
         }
-        console.log(data.location)
-        console.log(forecastData)
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
     })
-})
+} else {
+    console.log('You should provide a city: node app.js "MyCity"')
+}
